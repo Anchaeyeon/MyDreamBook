a268d5d9c363ccfe0ad0a3b36e4291368a2735a2
Chaeyeon Ahn <s2328@e-mirim.hs.kr>
2025-08-28 16:09:48 +0900

    #1 정보 폼 꾸미기 구현

--- FormAbout.Designer.cs
+++ FormAbout.Designer.cs
@@ -28,20 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MyDreamBook.Properties.Resources.MyDreamBook1;
+            this.pictureBox1.Location = new System.Drawing.Point(85, 91);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(232, 282);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(360, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(381, 54);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "이 프로그램은 이름과 꿈(직업)을 바탕으로 유명인이 된다면 미래에 자신이 이룰 업적과 어떤 삶을 살게 될지 보여주는 포츈텔러입니다.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(360, 292);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(353, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "요즘 미래에 대해 생각을 많이 하는 사람들이 사용하길 바랍니다.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(658, 361);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "버전 @19.01.7";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(360, 201);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(341, 40);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "NCP 수업에서 Winform을 활용한 포츈텔러 프로그램 입니다!사용한 언어는 C#입니다!";
             // 
             // FormAbout
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "FormAbout";
             this.Text = "FormAbout";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
