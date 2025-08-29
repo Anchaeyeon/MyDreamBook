deb961584feca103550cd5b28e65a746f05dd603
Chaeyeon Ahn <s2328@e-mirim.hs.kr>
2025-08-29 10:42:23 +0900

    #3 filename mydreambook으로 다시 변경

--- Form1.cs
+++ Form1.cs
@@ -24,7 +24,7 @@ namespace MyDreamBook
         {
             try
             {
-                string filename = "mybookdream_results.csv";
+                string filename = "mydreambook_results.csv";
                 results = File.ReadAllLines(filename).ToList();
             }
             catch (FileNotFoundException ex)
